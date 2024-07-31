90f36c8d134d4117b6e0b4f925e26b5f9a1a05ef
lduchesne <>
2024-07-31 15:22:00 +0200

    Fix de build pour yohan

---
+++ chatbot/src/test/kotlin/com/MainTest.kt
@@ -0,0 +1,13 @@
+// Chemin: src/test/kotlin/com/example/MainTest.kt
+package com.example
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun testMain() {
+        Assertions.assertEquals(3, 1 + 2)
+    }
+}
