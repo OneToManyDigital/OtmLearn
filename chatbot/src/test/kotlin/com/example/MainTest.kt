56989ab444e8af359cb61dbd786a2729592cb51c
Aironiss <yosa382@gmail.com>
2024-08-02 15:28:43 +0200

    Kotlin config

---
+++ chatbot/src/test/kotlin/com/example/MainTest.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun testMain(){
+        Assertions.assertEquals(2, 1+1)
+    }
+}
