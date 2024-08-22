7c6bef7f052454945c67b0aa2857d6d0233d5a11
Aironiss <yosa382@gmail.com>
2024-08-22 17:30:51 +0200

    Reactive
    Added reactive to endpoints.

--- chatbot/quarkusChatbot/src/main/kotlin/com/example/quarkusChatbot/ChatMessage.kt
+++ chatbot/quarkusChatbot/src/main/kotlin/com/example/quarkusChatbot/ChatMessage.kt
@@ -1,13 +1,15 @@
 package com.example.quarkusChatbot
 
-import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntity
+import io.quarkus.hibernate.reactive.panache.kotlin.PanacheEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import jakarta.persistence.Cacheable
 import jakarta.ws.rs.core.Response
 
 @Entity
 @Table(name = "conversation")
+@Cacheable
 data class ChatMessage(
     @Column(name = "user_message", length = 1000)
     var userMessage: String,
--- chatbot/quarkusChatbot/src/main/kotlin/com/example/quarkusChatbot/ChatbotResource.kt
+++ chatbot/quarkusChatbot/src/main/kotlin/com/example/quarkusChatbot/ChatbotResource.kt
@@ -1,15 +1,26 @@
 package com.example.quarkusChatbot
 
 import io.agroal.pool.MetricsRepository
+import io.quarkus.hibernate.reactive.panache.Panache
+import io.smallrye.common.annotation.NonBlocking
+import io.smallrye.common.annotation.Blocking
+import io.smallrye.common.annotation.RunOnVirtualThread
+import io.smallrye.mutiny.Multi
+import io.smallrye.mutiny.Uni
+import io.smallrye.mutiny.uni
 import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import jakarta.inject.Inject
 import jakarta.transaction.Transactional
+import jakarta.enterprise.context.ApplicationScoped
+import org.jboss.resteasy.reactive.RestResponse
 import javax.print.attribute.standard.Media
 import kotlin.random.Random
 
 @Path("/chat")
+@ApplicationScoped
+//@NonBlocking
 class ChatbotResource {
 
     private val responses = listOf("Hello !", "How can I help you ?", "Goodbye !")
@@ -21,21 +32,37 @@ class ChatbotResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
-    fun chat(message: Map<String, String>) : Response {
-        val userMessage = message["userMessage"] ?: return Response.status(Response.Status.BAD_REQUEST).build()
+    @NonBlocking
+    fun chat(message: Map<String, String>) : Uni<Response> {
+        //val userMessage = message["userMessage"] ?: return Response.status(Response.Status.BAD_REQUEST).build()
+        val userMessage = message["userMessage"] ?: return Uni.createFrom().item(Response.status(Response.Status.BAD_REQUEST).build())
         val botResponse = responses.random()
 
         val chatMessage = ChatMessage(userMessage = userMessage, botResponse = botResponse)
-        chatMessage.persist()
+        //chatMessage.persist()
 
-        return Response.ok(chatMessage).build()
+        //return Response.ok(chatMessage).build()
+        /*return Panache.withTransaction(chatMessage::persist)
+            .replaceWith(Response.status(Response.Status.CREATED)
+                .build())*/
+        return Panache.withTransaction(chatMessage::persist)
+            .replaceWith(Response.ok(chatMessage).build())
     }
+
+    /*@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @NonBlocking
+    fun getAllConversations(): Uni<Response> {
+        val conversations = repository.listAll()
+        //return Response.ok(conversations).build()
+        return Uni.createFrom().item(Response.ok(conversations).build())
+    }*/
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    fun getAllConversations(): Response {
+    @NonBlocking
+    fun getAllConversations(): Uni<Response> {
         val conversations = repository.listAll()
-        return Response.ok(conversations).build()
+        return Uni.createFrom().item(Response.ok(conversations).build())
     }
 }
